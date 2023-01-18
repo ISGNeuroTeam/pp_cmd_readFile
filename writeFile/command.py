acb8ff7bc9c6ab139c5c398ef127f2c44c6094bc
Artem Zenkov <azenkov@isgneuro.com>
2023-01-18 11:15:24 +0700

    write command

--- writeFile/command.py
+++ writeFile/command.py
@@ -1,10 +1,11 @@
 import pandas as pd
 from pathlib import Path
+
 from otlang.sdk.syntax import Keyword, Positional, OTLType
 from pp_exec_env.base_command import BaseCommand, Syntax
 
 
-class ReadfileCommand(BaseCommand):
+class WritefileCommand(BaseCommand):
     # define syntax of your command here
     syntax = Syntax(
         [
@@ -12,10 +13,9 @@ class ReadfileCommand(BaseCommand):
             Keyword("type", required=False, otl_type=OTLType.TEXT),
         ],
     )
-    use_timewindow = False  # Does not require time window arguments
-    idempotent = True  # Does not invalidate cache
 
     def transform(self, df: pd.DataFrame) -> pd.DataFrame:
+
         filename = self.get_arg("filename").value
         type = self.get_arg("type").value or filename.split('.')[-1]
 
@@ -23,21 +23,26 @@ class ReadfileCommand(BaseCommand):
             storage = self.config['storage']['path']
         else:
             storage = '/opt/otp/shared_storage/persistent_storage'
-
         Path(storage).mkdir(exist_ok=True, parents=True)
-
         full_file_path = Path(f'{storage}/{filename}')
-        # check file exists
-        if not full_file_path.exists():
-            raise ValueError('File not exists')
-        if type == 'json':
-            df = pd.read_json(full_file_path, lines=True)
-        elif type == 'parquet':
-            df = pd.read_parquet(
-                full_file_path, engine='pyarrow', use_pandas_metadata=True
+
+        if df is None:
+            df = pd.DataFrame()
+
+        self.log_progress(f'Start writing to {storage}/{filename}', stage=1, total_stages=2)
+        if type == 'parquet':
+            df.to_parquet(
+                full_file_path, engine='pyarrow', compression=None
+            )
+        elif type == 'json':
+            df.to_json(
+                full_file_path, orient='records', lines=True
             )
         elif type == 'csv':
-            df = pd.read_csv(full_file_path)
+            df.to_csv(
+                full_file_path
+            )
         else:
             raise ValueError('Unknown type')
+        self.log_progress(f'Writing is done {storage}/{filename}', stage=2, total_stages=2)
         return df
